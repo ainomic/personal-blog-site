5c67a92de97579c829b87e22518bf616e615cb61
Taniya Singhal <20190201051@dypiu.ac.in>
2024-02-20 15:39:25 +0530

    Aunthentication Commit

---
+++ personal_blog/ainomic_blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from ainomic_blog import views
+
+urlpatterns = [
+    path('',views.index,name='home'),
+    path('login',views.loginUser,name='login'),
+    path('signup',views.signUser,name='signup'),
+    path('logout',views.logoutUser,name='logout'),
+]
--- personal_blog/ainomic_blog/views.py
+++ personal_blog/ainomic_blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request,"blogpage.html")
+
+def loginUser(request):
+    context={"message":"Not valid"}
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request,user)
+            return redirect("/")
+        else:
+            # No backend authenticated the credentials
+            return render(request,"login.html",context)
+    return render(request,"login.html")
+
+def signUser(request):
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        user = User.objects.create_user(email,"-",password)
+        user.save()
+        return redirect("/login")
+    return render(request,"signup.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
