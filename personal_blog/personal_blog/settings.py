ccb9f7f2c779a11e15222dd4e05cc3c6390ef972
Taniya Singhal <taniyasinghal1421@gmail.com>
2024-02-22 12:41:03 +0530

    Migration commit

--- personal_blog/personal_blog/settings.py
+++ personal_blog/personal_blog/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'personal_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+        },
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': 'Password@1421',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
