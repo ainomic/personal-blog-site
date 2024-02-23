ce7905ac796cf8712c02c3c9ca66306cb05f1f36
Taniya Singhal <taniyasinghal1421@gmail.com>
2024-02-23 12:06:02 +0530

    pasword security commit

--- personal_blog/personal_blog/settings.py
+++ personal_blog/personal_blog/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +85,8 @@ DATABASES = {
             'init_command': 'SET default_storage_engine=INNODB',
         },
         'NAME': 'blogdb',
-        'USER': 'root',
-        'PASSWORD': 'Password@1421',
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
